ea3e8752744e233b9d17c9c618d434868a606b95
zachzhu2016 <zachzhu2016@gmail.com>
2020-11-13 12:51:31 -0500

    Bug fix for PlotGroup Component

--- src/graphs/PlotGroup.tsx
+++ src/graphs/PlotGroup.tsx
@@ -6,45 +6,62 @@ const LINE_PLOTS = ['Amps A', 'Amps B', 'Amps C', 'Power Fac A', 'Power Fac B', 
 const BAR_PLOTS = ['Daily Energy', 'Daily Power', 'Monthly Average Watts'];
 
 export const PlotGroup = props => {
+
     const [containerWidth, containerHeight] = props.size;
     const plots: any[] = [];
-    console.log(props.fixedData);
-    console.log(props.plotData);
 
     for (const title in props.plotData) {
-
         const rawData = props.plotData[title];
         const parsedData = rawData.filter(d => d['Value'] !== undefined && typeof d['Value'] === 'number');
-
         let plot;
+        
         if (LINE_PLOTS.includes(title)) {
+
             const lineData: any = [];
             const points = parsedData.map((d, i) => {
                 const time = i; //new Date(d['Timestamp']);
                 const usage = parseFloat(d['Value'].toFixed(2));
                 return {x: time, y: usage};
             });
+            
             lineData.push({id: title, color: 'hsl(249, 70%, 50%)', data: points});
+
             if (!lineData || !points || points.length == 0) continue;
-            plot = <LinePlot data={lineData} header={title} />;
+
+            plot = (<PlotContainer key={title} width={containerWidth} height={containerHeight}> 
+                        <LinePlot data={lineData} header={title} />
+                    </PlotContainer>);
+
         } else if (BAR_PLOTS.includes(title)) {
+
             const barData = parsedData.map(d => {
                 const time = new Date(d['Timestamp']).toLocaleDateString();
                 const usage = parseFloat(d['Value'].toFixed(2));
                 return {time, usage};
             });
+
             if (!barData || barData.length == 0) continue;
-            plot = <BarPlot data={barData} header={title} />;
+            
+            plot = (<PlotContainer key={title} width={containerWidth} height={containerHeight}> 
+                        <BarPlot data={barData} header={title} />;
+                    </PlotContainer>);
         }
+
         if (!plot) continue;
         plots.push(plot);
     }
 
     return (
-        <>
-            {plots.map(plot => (
-                <div style={{width: containerWidth, height: containerHeight, margin: 'auto'}}>{plot}</div>
-            ))}
-        </>
+        <div>
+            {plots}
+        </div>
+    );
+};
+
+const PlotContainer = props => {
+    return (
+        <div style={{width: props.width, height: props.height, margin: 'auto'}}>
+            {props.children}
+        </div>
     );
 };
