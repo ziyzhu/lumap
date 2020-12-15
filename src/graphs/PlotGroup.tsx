f4c9f2ccd9881a9bdd0ceacbcf89e2969442657d
zachzhu2016 <zachzhu2016@gmail.com>
2020-12-14 21:04:03 -0500

    Bug fix

--- src/graphs/PlotGroup.tsx
+++ src/graphs/PlotGroup.tsx
@@ -6,7 +6,6 @@ const LINE_PLOTS = ['Amps A', 'Amps B', 'Amps C', 'Power Fac A', 'Power Fac B', 
 const BAR_PLOTS = ['Daily Energy', 'Daily Power', 'Monthly Average Watts'];
 
 export const PlotGroup = props => {
-
     const [containerWidth, containerHeight] = props.size;
     const plots: any[] = [];
 
@@ -14,26 +13,25 @@ export const PlotGroup = props => {
         const rawData = props.plotData[title];
         const parsedData = rawData.filter(d => d['Value'] !== undefined && typeof d['Value'] === 'number');
         let plot;
-        
-        if (LINE_PLOTS.includes(title)) {
 
+        if (LINE_PLOTS.includes(title)) {
             const lineData: any = [];
             const points = parsedData.map((d, i) => {
                 const time = i; //new Date(d['Timestamp']);
                 const usage = parseFloat(d['Value'].toFixed(2));
                 return {x: time, y: usage};
             });
-            
+
             lineData.push({id: title, color: 'hsl(249, 70%, 50%)', data: points});
 
             if (!lineData || !points || points.length == 0) continue;
 
-            plot = (<PlotContainer key={title} width={containerWidth} height={containerHeight}> 
-                        <LinePlot data={lineData} header={title} />
-                    </PlotContainer>);
-
+            plot = (
+                <PlotContainer key={title} width={containerWidth} height={containerHeight}>
+                    <LinePlot data={lineData} header={title} />
+                </PlotContainer>
+            );
         } else if (BAR_PLOTS.includes(title)) {
-
             const barData = parsedData.map(d => {
                 const time = new Date(d['Timestamp']).toLocaleDateString();
                 const usage = parseFloat(d['Value'].toFixed(2));
@@ -41,27 +39,21 @@ export const PlotGroup = props => {
             });
 
             if (!barData || barData.length == 0) continue;
-            
-            plot = (<PlotContainer key={title} width={containerWidth} height={containerHeight}> 
-                        <BarPlot data={barData} header={title} />;
-                    </PlotContainer>);
+
+            plot = (
+                <PlotContainer key={title} width={containerWidth} height={containerHeight}>
+                    <BarPlot data={barData} header={title} />
+                </PlotContainer>
+            );
         }
 
         if (!plot) continue;
         plots.push(plot);
     }
 
-    return (
-        <div>
-            {plots}
-        </div>
-    );
+    return <div>{plots}</div>;
 };
 
 const PlotContainer = props => {
-    return (
-        <div style={{width: props.width, height: props.height, margin: 'auto'}}>
-            {props.children}
-        </div>
-    );
+    return <div style={{width: props.width, height: props.height, margin: 'auto'}}>{props.children}</div>;
 };
