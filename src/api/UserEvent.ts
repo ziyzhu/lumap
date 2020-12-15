3486139edcc330d47020a5779b2362b4e6ccf8b8
zachzhu2016 <zachzhu2016@gmail.com>
2020-12-15 16:34:42 -0500

    modified user event

--- src/api/UserEvent.ts
+++ src/api/UserEvent.ts
@@ -14,32 +14,38 @@ export enum UserEvent {
 /*
  * Handles callbacks for all types of user event
  */
-export const handleUserEvent = (matchingKey: string, event: UserEvent) => {
+export const handleUserEvent = (matchingKeys: string[], event: UserEvent) => {
   if (!IModelApp || !IModelApp.viewManager || !IModelApp.viewManager.selectedView || !BuildingMapper.current) {
     return;
   }
 
   const buildingMapper = BuildingMapper.current;
-  const ecId = buildingMapper.getEcFromKey(matchingKey);
+  const ecIds: any[] = [];
+  for (const matchingKey of matchingKeys) {
+      const ecId = buildingMapper.getEcFromKey(matchingKey);
+      ecIds.push(ecId);
+  }
   const manager = EmphasizeElementManager.current;
   const viewport = IModelApp.viewManager.selectedView;
   const emph = EmphasizeElements.getOrCreate(viewport);
 
-  switch (event) {
-    case UserEvent.ZoomIn:
-      viewport.zoomToElements(ecId, {animateFrustumChange: true});
-      break;
-    case UserEvent.Highlight:
-      emph.overrideElements(ecId, viewport, ColorDef.red);
-      break;
-    case UserEvent.Isolate:
-      emph.isolateElements(ecId, viewport);
-      break;
-    case UserEvent.Clear:
-      EmphasizeElements.clear(viewport);
-      break;
+  for (const ecId of ecIds) {
+      switch (event) {
+        case UserEvent.ZoomIn:
+          viewport.zoomToElements(ecId, {animateFrustumChange: true});
+          break;
+        case UserEvent.Highlight:
+          emph.overrideElements(ecId, viewport, ColorDef.red);
+          break;
+        case UserEvent.Isolate:
+          emph.isolateElements(ecId, viewport);
+          break;
+        case UserEvent.Clear:
+          EmphasizeElements.clear(viewport);
+          break;
 
-    default:
-      console.log('This event is not yet supported');
+        default:
+          console.log('This event is not yet supported');
+      }
   }
 };
